2ede4ef92b75a39cb17b065463e590cec5655bb9
Peter Bex <peter.bex@bevuta.com>
2025-12-12 17:01:49 +0600

    Rename GitPython to gitpython
    The old name was deprecated and breaks with nixpkgs 25.11

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   es = lib.escapeShellArg;
 
   standaloneLocker = pkgs.writers.writePython3Bin "standalone-clojure-nix-locker" {
-    libraries = [ pkgs.python3Packages.GitPython ];
+    libraries = [ pkgs.python3Packages.gitpython ];
     flakeIgnore = [
       "E501" # We don't care about lines being too long
       "W504" # Allow line breaks after binary operators for multi-line conditionals
